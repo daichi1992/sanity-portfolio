f539d6bc1d31c10a642e7895c172f134a897bfe1
daichi1992 <daichi.yamagishi1@gmail.com>
2021-02-21 18:10:20 +0900

    エラー回避のため一部削除

--- src/components/About.js
+++ src/components/About.js
@@ -26,19 +26,21 @@ export default function About() {
         <main className="relative">
             <img src={photo} alt="photo" className="absolute w-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
-                    <div className="text-lg flex flex-col justify-center">
-                        <h1 className="cursive text-6xl text-green-300 mb-4">
-                            Hey there. I'm{" "}
-                            <span className="text-green-100">{author.name}</span>
-                        </h1>
-                        <div className="prose lg:prose-lx text-white">
-                            <BlockContent blocks={author.bio} projectId="2xt98euq" dataset="production" />
-                        </div>
-                    </div>
-                </section>
+                
             </div>
         </main>
     )
 }
+
+// <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+//                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
+//                     <div className="text-lg flex flex-col justify-center">
+//                         <h1 className="cursive text-6xl text-green-300 mb-4">
+//                             Hey there. I'm{" "}
+//                             <span className="text-green-100">{author.name}</span>
+//                         </h1>
+//                         <div className="prose lg:prose-lx text-white">
+//                             <BlockContent blocks={author.bio} projectId="2xt98euq" dataset="production" />
+//                         </div>
+//                     </div>
+//                 </section>
